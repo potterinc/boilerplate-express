24fdd2af60ecfe196610e5293b5cec90351d139a
Arekhandia Harry <harry_p30@yahoo.com>
2023-11-27 20:15:52 +0100

    transforming json message to uppercase

--- myApp.js
+++ myApp.js
@@ -1,16 +1,22 @@
 let express = require('express');
+let dotenv = require('dotenv');
 let app = express();
+
+dotenv.config();
 
 const filePath = __dirname + '/views';
 
-app.use('/public', express.static(__dirname +'/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 // app.get('/' ,function(req, res){
 //   res.sendFile(filePath)
 // })
 
-app.get('/json', function (req, res){
-  res.json({message: 'Hello json'});
+app.get('/json', function (req, res) {
+  if (process.env.MESSAGE_STYLE == 'uppercase')
+    res.json({ message: ('Hello json').toUpperCase() });
+  else
+    res.json({message: 'Hello json'})
 })
 
 module.exports = app;
