b60d9b1b2bea6a1a12d22a545c9432bff1548ffa
Arekhandia Harry <harry_p30@yahoo.com>
2023-11-27 17:35:11 +0100

    setup static assets folder

--- myApp.js
+++ myApp.js
@@ -1,47 +1,13 @@
 let express = require('express');
 let app = express();
 
-console.log('Hello World');
-
 const filePath = __dirname + '/views/';
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/"));
 
 app.get('/', function (req, res) {
   res.sendFile(filePath);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = app;
