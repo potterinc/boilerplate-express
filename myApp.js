a967dcfcbfe6080aac8be4217f22206e7d93944b
Arekhandia Harry <harry_p30@yahoo.com>
2023-11-27 14:07:34 +0100

    serve static assets

--- myApp.js
+++ myApp.js
@@ -2,7 +2,11 @@ let express = require('express');
 let app = express();
 
 console.log('Hello World');
+
 const filePath = __dirname + '/views/';
+
+app.use(express.static(__dirname = "/public"));
+
 app.get('/', function (req, res) {
   res.sendFile(filePath);
 });
