ed6fb2b9b88e3c4c710f822fd49f3212c2a09d84
Arekhandia Harry <harry_p30@yahoo.com>
2023-11-27 20:49:18 +0100

    Implement middleware for timeserver

--- myApp.js
+++ myApp.js
@@ -21,7 +21,7 @@ app.get('/json', function (req, res) {
 })
 
 app.get('/now', function (req, res, next) {
-  req.time = new Date().toTimeString();
+  req.time = new Date().toString();
   next()
 }, function (req, res) {
   res.json({ time: req.time })
