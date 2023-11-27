211ed8aec7518499e1205d10080e56273a83b1ac
Arekhandia Harry <harry_p30@yahoo.com>
2023-11-27 14:00:44 +0100

    serve an HTML file

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ let express = require('express');
 let app = express();
 
 console.log('Hello World');
-
+const filePath = __dirname + '/views/';
 app.get('/', function (req, res) {
-  res.send('Hello Express');
+  res.sendFile(filePath);
 });
 
 
