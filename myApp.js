b000cd447bb461f122bc344cb52e5716acf5899d
Arekhandia Harry <harry_p30@yahoo.com>
2023-11-27 20:57:08 +0100

    Implementedecho server with route parameters

--- myApp.js
+++ myApp.js
@@ -6,26 +6,36 @@ dotenv.config();
 
 const filePath = __dirname + '/views';
 
-app.use(logger);
+app.use(logger); // Logger middleware
 app.use('/public', express.static(__dirname + '/public'));
 
-// app.get('/' ,function(req, res){
-//   res.sendFile(filePath)
-// })
 
+// Static server
+app.get('/' ,function(req, res){
+  res.sendFile(filePath)
+});
+
+// JSON server
 app.get('/json', function (req, res) {
   if (process.env.MESSAGE_STYLE == 'uppercase')
     res.json({ message: ('Hello json').toUpperCase() });
   else
     res.json({ message: 'Hello json' })
-})
+});
 
+// Time server
 app.get('/now', function (req, res, next) {
   req.time = new Date().toString();
-  next()
+  next();
 }, function (req, res) {
   res.json({ time: req.time })
 });
+
+// Echo server
+app.get('/:word/echo', function(req, res){
+  res.json({echo: req.params.word})
+})
+
 
 // Middleware function
 function logger(req, res, next) {
